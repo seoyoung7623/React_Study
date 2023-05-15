717a0c8c5f1790e035d1be2d3d1c2f11f27ed270
seoyoung7623 <seoyoung7623@naver.com>
2023-05-16 01:54:19 +0900

    리액트 리덕스 연습

---
+++ vlpt리액트/리액트 입문/리덕스/redux/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import TodosContainer from "./containers/TodosContainer";
+
+function App() {
+  return (
+    <div>
+      <TodosContainer />
+    </div>
+  );
+}
+
+export default App;
